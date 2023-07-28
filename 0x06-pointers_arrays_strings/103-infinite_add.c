eb13674096398ce5e41752af33c745e5bc046e4b
Aymane <aymanelaamimi86@gmail.com>
2023-07-27 17:08:33 -0700

    magic

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,25 +1,22 @@
-#include "main.h"
-void rev_string(char *n)
+#include <stdio.h>
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+int main()
 {
-int i = 0;
-int j = 0;
-char temp;
-while (*(n + i) != '\0')
-{
-i++;
-}
-i--;
-for (j = 0; j < i; j++, i--)
-{
-temp = *(n + j);
-*(n + j) = *(n + i);
-*(n + i) = temp;
-}
+char n1[] = "123456789";
+char n2[] = "987654321";
+int size_r = 20;
+char result[size_r];
+char *sum = infinite_add(n1, n2, result, size_r);
+if (sum)
+printf("Sum: %s\n", sum);
+else
+printf("Result cannot be stored in the buffer.\n");
+return 0;
 }
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int overflow = 0, i = 0, j = 0, digits = 0;
-int val1 = 0, val2 = 0, temp_tot = 0;
+int i = 0, j = 0, carry = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_sum = 0;
 while (*(n1 + i) != '\0')
 i++;
 while (*(n2 + j) != '\0')
@@ -27,8 +24,8 @@ j++;
 i--;
 j--;
 if (j >= size_r || i >= size_r)
-return (0);
-while (j >= 0 || i >= 0 || overflow == 1)
+return 0;
+while (j >= 0 || i >= 0 || carry == 1)
 {
 if (i < 0)
 val1 = 0;
@@ -38,21 +35,21 @@ if (j < 0)
 val2 = 0;
 else
 val2 = *(n2 + j) - '0';
-temp_tot = val1 + val2 + overflow;
-if (temp_tot >= 10)
-overflow = 1;
+temp_sum = val1 + val2 + carry;
+if (temp_sum >= 10)
+carry = 1;
 else
-overflow = 0;
+carry = 0;
 if (digits >= (size_r - 1))
-return (0);
-*(r + digits) = (temp_tot % 10) + '0';
+return 0;
+*(r + digits) = (temp_sum % 10) + '0';
 digits++;
 j--;
 i--;
 }
 if (digits == size_r)
-return (0);
+return 0;
 *(r + digits) = '\0';
-rev_string(r);
-return (r);
+rev_string(r); // Reverse the string to get the correct result
+return r;
 }
